fbab3901c302d83dad33e473577af29db397ce8b
Turupawn <ahmed.hn.43@gmail.com>
2017-04-25 09:20:32 -0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "Test.h"
+#include <iostream>
+using namespace std;
+
+int power(int base, int exponent)
+{
+  return -1;
+}
+
+int main ()
+{
+    //Funcion evaluadora
+    evaluar();
+    return 0;
+}
